d61b5df31955b2a7fc27b59da9cdc817870e8f27
Alexander Emelin <frvzmb@gmail.com>
2018-09-16 16:08:09 +0300

    update for Centrifugo v2

---
+++ django/core/context_processors.py
@@ -0,0 +1,15 @@
+import jwt
+from django.conf import settings
+
+
+def get_connection_token(user, info=''):
+    user_pk = str(user.pk) if user.is_authenticated else ""
+    return jwt.encode({"sub": user_pk}, settings.CENTRIFUGE_SECRET).decode()
+
+
+def main(request):
+    return dict(
+        CENTRIFUGE_SOCKJS_ENDPOINT=settings.CENTRIFUGE_ADDRESS + '/connection/sockjs',
+        CENTRIFUGE_WS_ENDPOINT=settings.CENTRIFUGE_ADDRESS + '/connection/websocket',
+        CENTRIFUGE_TOKEN=get_connection_token(request.user),
+    )
--- insecure_mode/README.md
+++
@@ -1,9 +0,0 @@
-Demo of using insecure mode of Centrifugo.
-
-Centrifugo must be started in insecure mode:
-
-```
-centrifugo --config=config.json --insecure
-```
-
-Make sure you have `publish: true` in Centrifugo configuration file to enable publishing messages to channel from client side.
